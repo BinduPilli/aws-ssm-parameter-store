34836e9f93466504ffccfe476efe8c4c6fe6e9fe
Bindu P <bindupilli1314@gmail.com>
2023-05-19 13:21:25 +0530

    aws_ssm

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name = "test.test1"
-  type = "String"
+  type = "SecureString"
   value = "hello world"
+  key_id = "5421463f-c430-4bbe-a944-10147187c056"
 }
